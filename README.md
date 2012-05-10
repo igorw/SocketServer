4eb03d7a64b946f1a941f7d01d90a4aa2f78cde6
Igor Wiedler <igor@wiedler.ch>
2012-05-11 01:17:07 +0200

    Remove old stuff, all is moved to react-php/react

--- README.md
+++
@@ -1,3 +0,0 @@
-# React
-
-This repository has moved, please go to [react-php/react](https://github.com/react-php/react)!
--- tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$loader = require __DIR__.'/../vendor/autoload.php';
-$loader->add('React\Tests', __DIR__);
-$loader->register();
